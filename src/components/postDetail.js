9f9ecf8fd05a2bc7c796215e6ba7c34274d5181c
FrankMwesigwa <frank.mwesigwa1@gmail.com>
2019-02-21 17:37:29 +0300

    use of action creators

--- src/components/postDetail.js
+++ src/components/postDetail.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deletePost } from '../actions/postActions';
 
  class PostDetail extends Component {
+
+    handleDelete = () => {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push("/");
+    }
 
   render() {
       const post = this.props.post ? (
           <div className="post">
-            <h4 className="center"> Post Id: {this.props.posts.id}</h4>
-            <h4 className="center"> Title: {this.props.posts.title}</h4>
-            <p>{this.props.posts.content}</p>
+            <h4 className="center"> Post Id: {this.props.post.id}</h4>
+            <h4 className="center"> Title: {this.props.post.title}</h4>
+            <p>{this.props.post.content}</p>
+            <div className="center">
+                <button className="btn-grey" onClick={this.handleDelete}> Delete Post </button>
+            </div>
           </div>
       ) : (
           <div className="center">Loading Post .... </div>
@@ -29,4 +38,11 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(PostDetail);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost : (id) => {dispatch(deletePost(id))}
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostDetail);
